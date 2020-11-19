b762f53d3560f174719b57d1de70df6961ad7613
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2020-11-19 15:03:27 +0100

    TaskThree completed

--- TaskThree.sql
+++ TaskThree.sql
@@ -1,3 +1,2 @@
 SELECT * FROM category;
-SELECT * FROM film
-RIGHT JOIN film_category ON film_category.film_id WHERE film_category.category_id = 11;
+SELECT * FROM film INNER JOIN film_category ON film.film_id = film_category.film_id WHERE film_category.category_id = 11;
